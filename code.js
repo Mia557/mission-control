150ad76055e029432b3cad1e52e1bde51f09d82b
Mia Goodwin <miagoodwin15@gmail.com>
2022-02-06 15:29:47 -0800

    fixed mistakes in code

--- code.js
+++ code.js
@@ -7,9 +7,9 @@ function checkCreds()  {
 
     if (fullName.length > 20 || fullName.length < 2) {
     document.getElementById("loginstatus").innerHTML = "Full name has invalid number of characters";
-        } else if (bageNumb > 999 | badgeNumb < 0) {
-document.getElementById("loginstatus").innerHTML = "Badge ID is an invalid number";
-        } else {
+    } else if (badgeNumb > 999 || badgeNumb < 0) {
+            document.getElementById("loginstatus").innerHTML = "Badge ID is an invalid number";
+    } else {
             alert ("access Granted, Welcome" + fullName + "!");
             location.replace("indexw4.html");
         }   

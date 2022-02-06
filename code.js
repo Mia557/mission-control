be4321e1b11d11cb01b7839a699d3baaf612ad02
Mia Goodwin <miagoodwin15@gmail.com>
2022-02-06 15:46:53 -0800

    added notes

--- code.js
+++ code.js
@@ -1,16 +1,22 @@
+   // add another else 
+
+   //login code
 function checkCreds()  {
     var firstName = document.getElementById("fName").value;
     var lastName = document.getElementById("lName").value;
     var badgeNumb = document.getElementById("badgeID").value;
     var fullName = firstName + " " + lastName;
-} 
+
+//allow or decline user code
 
     if (fullName.length > 20 || fullName.length < 2) {
     document.getElementById("loginstatus").innerHTML = "Full name has invalid number of characters";
     } else if (badgeNumb > 999 || badgeNumb < 0) {
             document.getElementById("loginstatus").innerHTML = "Badge ID is an invalid number";
+
+// moving to next page
     } else {
-            alert ("access Granted, Welcome" + fullName + "!");
+            alert ("You're here! :D" + fullName + "!");
             location.replace("indexw4.html");
-        }   
-        // add another else and fix mistake
+    }
+}

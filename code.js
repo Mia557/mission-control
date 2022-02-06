69e71d021be66a9e70c7668bcf59ecb7117e3b88
Mia Goodwin <miagoodwin15@gmail.com>
2022-02-06 03:41:09 -0800

    added java function

---
+++ code.js
@@ -0,0 +1,6 @@
+function checkCreds()  {
+    var firstName = document.getElementById("fName").value;
+    var lastName = document.getElementById("lName").value;
+    var badgeNumb = document.getElementById("badgeID").value;
+    var fullName = firstName + " " + lastName;
+} 
